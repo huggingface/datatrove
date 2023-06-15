a41547a3a15ff896995b518f25d5a3d1d6b91927
alessandro cappelli <alessandro@lighton.ai>
2023-06-15 13:27:40 +0200

    Refactor gopher quality filter

--- src/datatrove/pipeline/filters/gopher_quality.py
+++ src/datatrove/pipeline/filters/gopher_quality.py
@@ -2,8 +2,8 @@ import numpy as np
 
 from nltk.tokenize import word_tokenize
 
-from datatrove import Document
-from datatrove.pipeline.filters.base import BaseFilter, FilterResult
+from datatrove.data import Document
+from datatrove.pipeline.filters.base import BaseFilter
 
 STOP_WORDS = ["the", "be", "to", "of", "and", "that", "have", "with"]
 
@@ -40,7 +40,7 @@ class GopherQuality(BaseFilter):
         @param min_stop_words:
         @param stop_words:
         """
-        super(GopherQuality, self).__init__(*args, **kwargs)
+        super(GopherQuality, self).__init__(**kwargs)
         self.min_doc_words = min_doc_words
         self.max_doc_words = max_doc_words
         self.min_avg_word_length = min_avg_word_length
@@ -52,7 +52,7 @@ class GopherQuality(BaseFilter):
         self.min_stop_words = min_stop_words
         self.stop_words = STOP_WORDS if stop_words is None else stop_words
 
-    def filter(self, doc: Document) -> FilterResult:
+    def filter(self, doc: Document) -> bool | tuple[bool, str]:
         """
             Applies the heuristics rules to decide if a document should be REMOVED:
                 -
@@ -66,40 +66,40 @@ class GopherQuality(BaseFilter):
         # words < min_doc_words or words > max_doc_words
         n_words = len(words)
         if n_words < self.min_doc_words and self.min_doc_words:
-            return FilterResult(False, "gopher_short_doc")
+            return False, "gopher_short_doc"
         if n_words > self.max_doc_words and self.max_doc_words:
-            return FilterResult(False, "gopher_long_doc")
+            return False, "gopher_long_doc"
 
         # mean word length is outside the range of 3 to 10 characters
         avg_n_words = np.mean([len(w) for w in words if w != "."])  # TODO check
         if avg_n_words < self.min_avg_word_length and self.min_avg_word_length:
-            return FilterResult(False, "gopher_below_avg_threshold")
+            return False, "gopher_below_avg_threshold"
         if avg_n_words > self.max_avg_word_length and self.max_avg_word_length:
-            return FilterResult(False, "gopher_above_avg_threshold")
+            return False, "gopher_above_avg_threshold"
 
         # symbol-to-word ratio greater than 0.1 for either the hash symbol or the ellipsis
         if text.count("#") / n_words > self.max_symbol_word_ratio and self.max_symbol_word_ratio:
-            return FilterResult(False, "gopher_too_many_hashes")
+            return False, "gopher_too_many_hashes"
         if text.count("...") / n_words > self.max_symbol_word_ratio and self.max_symbol_word_ratio:
-            return FilterResult(False, "gopher_too_many_ellipsis")
+            return False, "gopher_too_many_ellipsis"
 
         # any document with more than 90 % of lines starting with a bullet point,
         # or more than 30 % ending with an ellipsis.
         sentences = text.splitlines()  # TODO we can add language to sentence tokenizer
         if sum(s.startswith("•") for s in
                sentences) / n_words > self.max_bullet_lines_ratio and self.max_bullet_lines_ratio:
-            return FilterResult(False, "gopher_too_many_bullets")
+            return False, "gopher_too_many_bullets"
         if sum(s.endswith("...") for s in
                sentences) / n_words > self.max_ellipsis_lines_ratio and self.max_symbol_word_ratio:
-            return FilterResult(False, "gopher_too_many_end_ellipsis")
+            return False, "gopher_too_many_end_ellipsis"
 
         # that 80 % of words in a document contain at least one alphabetic character
         if sum([any([c.isalpha() for c in w]) for w in
-                words]) / n_words < self.max_non_alpha_words_ratio and self.max_non_alphabetic_words_ratio:
-            return FilterResult(False, "gopher_below_alpha_threshold")
+                words]) / n_words < self.max_non_alpha_words_ratio and self.max_non_alpha_words_ratio:
+            return False, "gopher_below_alpha_threshold"
 
         # stop word filter
         if sum(w in self.stop_words for w in words) < self.min_stop_words and self.min_stop_words:
-            return FilterResult(False, "gopher_enough_stop_words")
+            return False, "gopher_enough_stop_words"
 
-        return FilterResult(True, None)
+        return True
