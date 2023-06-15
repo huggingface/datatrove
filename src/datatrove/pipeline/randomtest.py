dfe16549ec996a8a9d8fdba91a7741ce1c00f9c0
guipenedo <nostrumg@gmail.com>
2023-06-15 13:17:03 +0200

    bugfix

--- src/datatrove/pipeline/randomtest.py
+++ src/datatrove/pipeline/randomtest.py
@@ -9,7 +9,7 @@ class RandomTest(PipelineStep):
     def __init__(self, name):
         self.name = name
 
-    def __call__(self, rank: int, world_size: int, data: DocumentsPipeline) -> DocumentsPipeline:
+    def __call__(self, data: DocumentsPipeline, rank: int = 0, world_size: int = 1) -> DocumentsPipeline:
         print(f"STARTING {self.name=}, {rank=}, {world_size=}")
         time.sleep(random.randint(2, 5))
         print(f"COMPLETED {self.name=}, {rank=}, {world_size=}")
