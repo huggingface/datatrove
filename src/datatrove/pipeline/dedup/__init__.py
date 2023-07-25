ab9ae3c16a11155f693ac0f70960da130e9ad757
guipenedo <nostrumg@gmail.com>
2023-07-25 16:24:03 +0200

    added module imports

--- src/datatrove/pipeline/dedup/__init__.py
+++ src/datatrove/pipeline/dedup/__init__.py
@@ -1,2 +1,3 @@
+from .minhash import MinhashDedupBuckets, MinhashDedupCluster, MinhashDedupFilter, MinhashDedupSignature
 from .exact_substrings import DatasetToSequence, DedupReader, MergeSequences
 from .sentence_dedup import SentenceDedupFilter, SentenceDedupSignature, SentenceFindDedups
