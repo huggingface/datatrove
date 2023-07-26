3581a1871d96f2f30a4158d7eca04493401433f7
guipenedo <nostrumg@gmail.com>
2023-07-26 18:23:31 +0200

    fix import sorting

--- src/datatrove/pipeline/dedup/__init__.py
+++ src/datatrove/pipeline/dedup/__init__.py
@@ -1,3 +1,3 @@
-from .minhash import MinhashDedupBuckets, MinhashDedupCluster, MinhashDedupFilter, MinhashDedupSignature
 from .exact_substrings import DatasetToSequence, DedupReader, MergeSequences
+from .minhash import MinhashDedupBuckets, MinhashDedupCluster, MinhashDedupFilter, MinhashDedupSignature
 from .sentence_dedup import SentenceDedupFilter, SentenceDedupSignature, SentenceFindDedups
