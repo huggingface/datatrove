7485bec35f7ef5a0d171cdc21c50bca961d36067
alessandro cappelli <alessandro@lighton.ai>
2023-08-04 10:40:18 +0200

    :sparkles: Add list filter

--- src/datatrove/pipeline/filters/__init__.py
+++ src/datatrove/pipeline/filters/__init__.py
@@ -2,6 +2,7 @@ from .gopher_quality_filter import GopherQualityFilter
 from .gopher_repetition_filter import GopherRepetitionFilter
 from .lambda_filter import LambdaFilter
 from .language_filter import LanguageFilter
+from .list_filter import ListFilter
 from .regex_filter import RegexFilter
 from .unigram_log_probs import UnigramLogProbFilter
 from .url_filter import URLFilter
