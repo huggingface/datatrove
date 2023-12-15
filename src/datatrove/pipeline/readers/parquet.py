46750dd513da4189f57dd3a4ea665412a879a70c
Mario Šaško <mariosasko777@gmail.com>
2023-12-15 20:44:34 +0100

    Optimize `ParquetReader` (#40)
    * Optimize `ParquetReader`
    
    * Address comments
    
    * correctly track time
    
    ---------
    
    Co-authored-by: guipenedo <nostrumg@gmail.com>

--- src/datatrove/pipeline/readers/parquet.py
+++ src/datatrove/pipeline/readers/parquet.py
@@ -13,6 +13,8 @@ class ParquetReader(BaseReader):
         self,
         data_folder: BaseInputDataFolder,
         limit: int = -1,
+        batch_size: int = 1000,
+        read_metadata: bool = True,
         progress: bool = False,
         adapter: Callable = None,
         content_key: str = "content",
@@ -20,13 +22,21 @@ class ParquetReader(BaseReader):
         default_metadata: dict = None,
     ):
         super().__init__(data_folder, limit, progress, adapter, content_key, id_key, default_metadata)
+        self.batch_size = batch_size
+        self.read_metadata = read_metadata
 
     def read_file(self, datafile: BaseInputDataFile):
         with datafile.open(binary=True) as f:
             with pq.ParquetFile(f) as pqf:
-                for li, line in enumerate(pqf.iter_batches(batch_size=1)):
-                    with self.track_time():
-                        document = self.get_document_from_dict(line.to_pydict(), datafile, li)
-                        if not document:
-                            continue
-                    yield document
+                li = 0
+                columns = [self.content_key, self.id_key] if not self.read_metadata else None
+                for batch in pqf.iter_batches(batch_size=self.batch_size, columns=columns):
+                    documents = []
+                    with self.track_time("batch"):
+                        for line in batch.to_pylist():
+                            document = self.get_document_from_dict(line, datafile, li)
+                            if not document:
+                                continue
+                            documents.append(document)
+                            li += 1
+                    yield from documents
