bec14a4644d2e0886f63a6c0f14fccf50603981e
mariosasko <mariosasko777@gmail.com>
2023-12-18 20:58:48 +0100

    Add Feather reader

---
+++ src/datatrove/pipeline/readers/feather.py
@@ -0,0 +1,43 @@
+from typing import Callable
+
+import pyarrow as pa
+
+from datatrove.io import BaseInputDataFile, BaseInputDataFolder
+from datatrove.pipeline.readers.base import BaseReader
+
+
+class ParquetReader(BaseReader):
+    name = "📒 Parquet"
+
+    def __init__(
+        self,
+        data_folder: BaseInputDataFolder,
+        limit: int = -1,
+        batch_size: int = 1000,
+        read_metadata: bool = True,
+        progress: bool = False,
+        adapter: Callable = None,
+        content_key: str = "content",
+        id_key: str = "data_id",
+        default_metadata: dict = None,
+    ):
+        super().__init__(data_folder, limit, progress, adapter, content_key, id_key, default_metadata)
+        self.batch_size = batch_size
+        self.read_metadata = read_metadata
+
+    def read_file(self, datafile: BaseInputDataFile):
+        with datafile.open(binary=True) as f:
+            columns = [self.content_key, self.id_key] if not self.read_metadata else None
+            read_options = pa.ipc.IpcReadOptions(fields=columns)
+            with pa.ipc.open_file(f, read_options=read_options) as feather_reader:
+                li = 0
+                for batch in feather_reader:
+                    documents = []
+                    with self.track_time("batch"):
+                        for line in batch.to_pylist():
+                            document = self.get_document_from_dict(line, datafile, li)
+                            if not document:
+                                continue
+                            documents.append(document)
+                            li += 1
+                    yield from documents
