0c133a447df406a8574e48264a7c1aaa443535d9
guipenedo <nostrumg@gmail.com>
2024-02-22 15:33:22 +0100

    change to PRs

--- src/datatrove/pipeline/writers/huggingface.py
+++ src/datatrove/pipeline/writers/huggingface.py
@@ -1,11 +1,20 @@
 import os
 import tempfile
+import time
 from typing import Callable
 
 from fsspec.implementations.local import LocalFileSystem
-from huggingface_hub import CommitOperationAdd, create_commit, create_repo, preupload_lfs_files
+from huggingface_hub import (
+    CommitOperationAdd,
+    create_commit,
+    create_repo,
+    get_repo_discussions,
+    merge_pull_request,
+    preupload_lfs_files,
+)
 from loguru import logger
 
+from datatrove.data import DocumentsPipeline
 from datatrove.io import DataFolderLike, get_datafolder
 from datatrove.pipeline.writers import ParquetWriter
 
@@ -66,5 +75,39 @@ class HuggingFaceDatasetWriter(ParquetWriter):
                 self.local_working_dir.rm(file)
             operations.append(addition)
         create_commit(
-            repo_id, repo_type="dataset", operations=operations, commit_message=f"Commit {len(filelist)} files."
+            repo_id,
+            repo_type="dataset",
+            operations=operations,
+            commit_message=f"DataTrove upload ({len(filelist)} files)",
+            create_pr=True,
         )
+
+    def _get_open_datatrove_prs(self):
+        return filter(
+            lambda x: x.title.startswith("DataTrove upload"),
+            get_repo_discussions(
+                repo_id=self.dataset,
+                repo_type="dataset",
+                discussion_type="pull_request",
+                discussion_status="open",
+            ),
+        )
+
+    def run(self, data: DocumentsPipeline, rank: int = 0, world_size: int = 1) -> DocumentsPipeline:
+        super().run(data, rank, world_size)
+        if rank == 0:
+            # wait for all the PRs to have been submitted
+            while True:
+                nr_open_datatrove_prs = len(list(self._get_open_datatrove_prs()))
+                if nr_open_datatrove_prs == world_size:
+                    break
+                logger.info(f"Found {nr_open_datatrove_prs}/{world_size} open datatrove PRs, waiting...")
+                time.sleep(30)
+            # merge all the PRs
+            for discussion in self._get_open_datatrove_prs():
+                merge_pull_request(
+                    repo_id=self.dataset,
+                    repo_type="dataset",
+                    discussion_num=discussion.num,
+                    comment="Auto merged by DataTrove",
+                )
