cc5cff12c7cf35018bcc42f5a95a548fb091f292
guipenedo <nostrumg@gmail.com>
2024-03-22 19:19:27 +0100

    added binary utils

--- src/datatrove/utils/binaryio.py
+++ src/datatrove/utils/binaryio.py
@@ -1,31 +1,9 @@
-import hashlib
 import os
-import re
 import struct
-import unicodedata
 from functools import cache
 from typing import BinaryIO
 
 from fsspec.spec import AbstractBufferedFile
-
-
-class ExtensionHelperSD:
-    stage_1_signature = ".c4_sig"
-    stage_2_duplicates = ".c4_dup"
-    index = ".c4_index"
-
-
-class ExtensionHelperES:
-    stage_1_sequence = ".es_sequence"
-    stage_1_sequence_size = ".es_sequence.size"
-    stage_2_big_sequence = ".big_sequence"
-    stage_2_bytes_offset = ".info"
-    stage_3_bytes_ranges = ".bytearange"
-
-
-PUNCTUATION = "!/—”:％１〈&(、━\\【#%「」，】；+^]~“《„';’{|∶´[=-`*．（–？！：$～«〉,><》)?）。…@_.\"}►»" + "".join(
-    map(chr, list(range(0, 32)) + list(range(127, 160)))
-)
 
 
 def read_tuples_from_file(file: BinaryIO, *formats, lines_to_buffer: int = 5):
@@ -49,56 +27,6 @@ def read_tuples_from_file(file: BinaryIO, *formats, lines_to_buffer: int = 5):
         if not chunk:
             break
         yield from reader.iter_unpack(chunk)
-
-
-def simplify_text(text: str) -> str:
-    """Performs the following operations to increase recall when looking for matches between documents:
-    - lowercase text
-    - replace all whitespace with a single " "
-    - remove all punctuation
-    - convert diacritics
-    - unicode normalize
-
-    Args:
-        text
-
-    Returns:
-        modified text
-    """
-    # lower case
-    text = text.lower()
-    # remove consecutive spaces, newlines, tabs in the middle and in the beginning / end
-    text = re.sub(r"\s+", " ", text.strip())
-    # remove punctuation
-    text = text.translate(str.maketrans("", "", PUNCTUATION))
-    # diacritics/unicode normalization
-    text = "".join(c for c in unicodedata.normalize("NFD", text) if unicodedata.category(c) != "Mn")
-    return text.strip()
-
-
-# https://github.com/ekzhu/datasketch/blob/master/datasketch/hashfunc.py
-def sha1_hash32(data):
-    """A 32-bit hash function based on SHA1.
-
-    Args:
-        data (bytes): the data to generate 32-bit integer hash from.
-
-    Returns:
-        int: an integer hash value that can be encoded using 32 bits.
-    """
-    return struct.unpack("<I", hashlib.sha1(data).digest()[:4])[0]
-
-
-def sha1_hash64(data):
-    """A 64-bit hash function based on SHA1.
-
-    Args:
-        data (bytes): the data to generate 64-bit integer hash from.
-
-    Returns:
-        int: an integer hash value that can be encoded using 64 bits.
-    """
-    return struct.unpack("<Q", hashlib.sha1(data).digest()[:8])[0]
 
 
 def seek_to_start(f: AbstractBufferedFile, start_hash: int, line_format: str, hash_format: str):
