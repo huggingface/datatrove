5933335f1ae554de2786310be00af1b5df05d6a3
vsabolcec <vinko.sabolcec1@gmail.com>
2024-04-03 14:17:21 +0200

    English tokenizer test

--- tests/pipeline/test_word_tokenizers.py
+++ tests/pipeline/test_word_tokenizers.py
@@ -1,16 +1,31 @@
 import unittest
 
-from datatrove.tools.word_tokenizers import WORD_TOKENIZERS
+from nltk.tokenize import word_tokenize
+
+from datatrove.tools.word_tokenizers import WORD_TOKENIZERS, get_word_tokenizer
+from datatrove.utils.typeshelper import Languages
 
 
-sample_text = "Hello world! \n\n ქართული \n\t Hello\nworld! " * 50
+SAMPLE_TEXT = (
+    "I wish it need not have happened in my time,' said Frodo. 'So do I,' said Gandalf, 'and so do all who live to "
+    "see such times. But that is not for them to decide. All we have to decide is what to do with the time that is "
+    "given us.'"
+)
 
 
 class TestWordTokenizers(unittest.TestCase):
     def test_word_tokenizers(self):
         for language in WORD_TOKENIZERS:
             tokenizer = WORD_TOKENIZERS[language]
-            tokens = tokenizer.tokenize(sample_text)
+            tokens = tokenizer.tokenize(SAMPLE_TEXT)
             assert len(tokens) >= 1, f"'{language}' tokenizer assertion failed"
             is_stripped = [token == token.strip() for token in tokens]
             assert all(is_stripped), f"'{language}' tokens contain whitespaces"
+
+    def test_english_tokenizer(self):
+        nltk_words = word_tokenize(SAMPLE_TEXT, language="english")
+
+        tokenizer = get_word_tokenizer(Languages.english)
+        tokenizer_words = tokenizer.tokenize(SAMPLE_TEXT)
+
+        self.assertEqual(nltk_words, tokenizer_words, "NLTK tokenizer and multilingual tokenizer differ")
