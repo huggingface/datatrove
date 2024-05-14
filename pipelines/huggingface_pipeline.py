909f036173169a7cbb3acfe757f8d93063997e4b
Emanuela Boros <emanuela.boros@gmail.com>
2024-05-14 14:21:12 +0200

    formatting

--- pipelines/huggingface_pipeline.py
+++ pipelines/huggingface_pipeline.py
@@ -1,6 +1,7 @@
 """
 
 """
+
 import os, re
 from datatrove.pipeline.readers.huggingface import HuggingFaceDatasetReader
 from datatrove.pipeline.tokens import TokensCounter, LengthCounter
@@ -8,12 +9,14 @@ from swiss_ai.writers.jsonl import SwissAIJsonlWriter
 from datatrove.executor.local import LocalPipelineExecutor
 
 os.environ["HF_BASE"] = "/work_space_data/hf_cache"
+if not os.path.exists("/work_space_data/hf_cache"):
+    os.makedirs("/work_space_data/hf_cache")
 
 
 def find_years(text):
     # Regex pattern to match four-digit numbers that are likely to be years
     # This pattern matches any number from 1900 to 2099
-    pattern = r'\b(19[0-9]{2}|20[0-9]{2})\b'
+    pattern = r"\b(19[0-9]{2}|20[0-9]{2})\b"
 
     # Find all matches in the text
     years = re.findall(pattern, text)
@@ -22,48 +25,43 @@ def find_years(text):
 
 
 def _multilegal_adapter(data: dict, path: str, id_in_file: int | str):
-    years = find_years(data['text'])
+    years = find_years(data["text"])
     if len(years) > 0:
-        #very crude estimation of the year..
+        # very crude estimation of the year..
         year = max(int(year) for year in years if int(year) <= 2024)
     else:
         year = 2024
     metadata = {
-        'language': data['language'],
-        'year': year,
-        'optional': {
-            'type': data['type'],
-            'jurisdiction': data['jurisdiction']
-        }
+        "language": data["language"],
+        "year": year,
+        "optional": {"type": data["type"], "jurisdiction": data["jurisdiction"]},
     }
 
     return {
-        "text": data.pop('text', ""),
+        "text": data.pop("text", ""),
         "id": f"{path}/{id_in_file}",
         "media": data.pop("media", []),
-        "metadata": metadata
+        "metadata": metadata,
     }
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pipeline = [
         HuggingFaceDatasetReader(
-            dataset='joelniklaus/Multi_Legal_Pile',
+            dataset="joelniklaus/Multi_Legal_Pile",
             dataset_options={
-                'split': 'train',
-                'name': 'da_caselaw',
-                'cache_dir': '/work_space_data/hf_cache',
-                'trust_remote_code': True
+                "split": "train",
+                "name": "da_caselaw",
+                "cache_dir": "/work_space_data/hf_cache",
+                "trust_remote_code": True,
             },
             progress=True,
             adapter=_multilegal_adapter,
-            limit=1000
+            limit=1000,
         ),
-        TokensCounter(tokenizer_name_or_path='t5-small'),
+        TokensCounter(),
         LengthCounter(),
-        SwissAIJsonlWriter(
-            output_folder="/work_space_data/multilegal_pile/jsonl"
-        )
+        SwissAIJsonlWriter(output_folder="/work_space_data/multilegal_pile/jsonl"),
     ]
 
     exec = LocalPipelineExecutor(
@@ -71,7 +69,7 @@ if __name__ == '__main__':
         tasks=16,
         workers=1,
         start_method="spawn",
-        logging_dir="/work_space_data/multilegal_pile/logging"
+        logging_dir="/work_space_data/multilegal_pile/logging",
     )
 
     exec.run()
--- pipelines/swissdox_raw.py
+++ pipelines/swissdox_raw.py
@@ -6,20 +6,18 @@ from swiss_ai.readers.swissdox import RawSwissDoxReader
 from datatrove.pipeline.tokens import TokensCounter, LengthCounter
 from datatrove.pipeline.writers import JsonlWriter
 from datatrove.executor.local import LocalPipelineExecutor
+import os
 
 os.environ["HF_BASE"] = "/work_space_data/hf_cache/"
+if not os.path.exists("/work_space_data/swissdox"):
+    os.makedirs("/work_space_data/swissdox")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pipeline = [
-        RawSwissDoxReader(
-            data_folder="/work_space_data/swissdox",
-            limit=-1
-        ),
-        TokensCounter(tokenizer_name_or_path='t5-small'),
+        RawSwissDoxReader(data_folder="/work_space_data/swissdox", limit=-1),
+        TokensCounter(tokenizer_name_or_path="t5-small"),
         LengthCounter(),
-        JsonlWriter(
-            output_folder="/work_space_data/swissdox/jsonl"
-        )
+        JsonlWriter(output_folder="/work_space_data/swissdox/jsonl"),
     ]
 
     exec = LocalPipelineExecutor(
@@ -27,7 +25,7 @@ if __name__ == '__main__':
         tasks=16,
         workers=16,
         start_method="spawn",
-        logging_dir="/work_space_data/swissdox/logging"
+        logging_dir="/work_space_data/swissdox/logging",
     )
 
     exec.run()
