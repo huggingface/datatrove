44519b8d2e921a37067832704926274ad681395b
Hynek Kydlicek <kydlicek.hynek@huggingface.co>
2024-06-14 08:42:54 +0000

    join normalization techniques for preplexity

--- src/datatrove/utils/perplexity.py
+++ src/datatrove/utils/perplexity.py
@@ -9,6 +9,7 @@ from typing import Dict
 from huggingface_hub import hf_hub_url
 
 from datatrove.io import cached_asset_path_or_download
+from datatrove.utils.text import TextNormConfig, simplify_text
 
 
 MODEL_REPO = "edugp/kenlm"
@@ -87,17 +88,9 @@ class KenlmModel:
         self,
         model_dataset: str,
         language: str,
-        lower_case: bool = False,
-        remove_accents: bool = False,
-        normalize_numbers: bool = True,
-        punctuation: int = 1,
     ):
         self.model_dataset = model_dataset
         self.language = language
-        self.accent = remove_accents
-        self.case = lower_case
-        self.numbers = normalize_numbers
-        self.punct = punctuation
         self._tokenizer = None
         self._model = None
 
@@ -126,10 +119,6 @@ class KenlmModel:
         return cls(
             model_dataset,
             language,
-            False,
-            False,
-            True,
-            1,
         )
 
     def pp(self, log_score, length):
@@ -139,10 +128,6 @@ class KenlmModel:
         if normalize_cc_net:
             doc = self.normalize(
                 doc,
-                accent=self.accent,
-                case=self.case,
-                numbers=self.numbers,
-                punct=self.punct,
             )
         # Tokenize (after normalizing): See https://github.com/facebookresearch/cc_net/blob/bda555bd1cf1ee2e0b925363e62a61cd46c8b60d/cc_net/mine.py#L352 for full pipeline
         doc = self.tokenizer.tokenize(doc)
@@ -156,42 +141,22 @@ class KenlmModel:
 
     def normalize(
         self,
-        line: str,
-        accent: bool = True,
-        case: bool = True,
-        numbers: bool = True,
-        punct: int = 1,
+        text: str,
     ) -> str:
-        line = line.strip()
-        if not line:
-            return line
-        if case:
-            line = line.lower()
-        if accent:
-            line = self.strip_accents(line)
-        if numbers:
-            line = self.digit_re.sub("0", line)
-        if punct == 1:
-            line = self.replace_unicode_punct(line)
-        elif punct == 2:
-            line = self.remove_unicode_punct(line)
-        line = self.remove_non_printing_char(line)
-        return line
-
-    def strip_accents(self, line: str) -> str:
-        """Strips accents from a piece of text."""
-        nfd = unicodedata.normalize("NFD", line)
-        output = [c for c in nfd if unicodedata.category(c) != "Mn"]
-        if len(output) == line:
-            return line
-        return "".join(output)
+        text = simplify_text(text, config=TextNormConfig(
+            lowercase=True,
+            norm_numbers=True,
+            norm_whitespace=False,
+            remove_punctuation=False,
+            norm_unicode_diacritics=True
+        ))
+        # TODO: integrate these options to simplify_text
+        text = self.replace_unicode_punct(text)
+        text = self.remove_non_printing_char(text)
+        return text
 
     def replace_unicode_punct(self, text: str) -> str:
         return "".join(self.unicode_punct.get(c, c) for c in text)
-
-    def remove_unicode_punct(self, text: str) -> str:
-        """More aggressive version of replace_unicode_punct but also faster."""
-        return self.unicode_punct_re.sub("", text)
 
     def remove_non_printing_char(self, text: str) -> str:
         return self.non_printing_chars_re.sub("", text)
