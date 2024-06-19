b74bc3283140632027dfeff9b72ad39aaa46b239
Guilherme Penedo <nostrumg@gmail.com>
2024-06-19 16:34:14 +0200

    Update src/datatrove/pipeline/readers/csv.py
    Co-authored-by: Hynek Kydlíček <kydlicek.hynek@gmail.com>

--- src/datatrove/pipeline/readers/csv.py
+++ src/datatrove/pipeline/readers/csv.py
@@ -34,7 +34,7 @@ class CsvReader(BaseDiskReader):
     def __init__(
         self,
         data_folder: DataFolderLike,
-        paths_file: DataFileLike = None,
+        paths_file: DataFileLike | None = None,
         compression: Literal["infer", "gzip", "zstd"] | None = "infer",
         limit: int = -1,
         skip: int = 0,
