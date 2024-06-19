18f5f9863a188ea5096f8500fb7c9397aabe833e
guipenedo <nostrumg@gmail.com>
2024-06-19 16:35:33 +0200

    fix typo

--- src/datatrove/pipeline/readers/warc.py
+++ src/datatrove/pipeline/readers/warc.py
@@ -38,7 +38,7 @@ class WarcReader(BaseDiskReader):
     def __init__(
         self,
         data_folder: DataFolderLike,
-        paths_file | None: DataFileLike = None,
+        paths_file: DataFileLike | None = None,
         compression: Literal["infer", "gzip", "zstd"] | None = "infer",
         limit: int = -1,
         skip: int = 0,
