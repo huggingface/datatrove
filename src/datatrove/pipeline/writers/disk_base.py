5998f0fb6b5168902c6d9a38296e1ce16987cd5b
justhungryman <justhungryman@gmail.com>
2024-06-23 00:42:40 +0900

    feat: Update DiskWriter to handle the other compression for Parquet files

--- src/datatrove/pipeline/writers/disk_base.py
+++ src/datatrove/pipeline/writers/disk_base.py
@@ -40,14 +40,13 @@ class DiskWriter(PipelineStep, ABC):
         self.compression = compression
         self.output_folder = get_datafolder(output_folder)
         output_filename = output_filename or self.default_output_filename
-        if self.compression == "gzip" and not output_filename.endswith(".gz"):
+        if output_filename.endswith(".parquet") and compression != "none":
+            output_filename = output_filename.replace(".parquet", f".{compression}.parquet")
+            compression = None  # compression is already handled by ParquetWriter
+        elif self.compression == "gzip" and not output_filename.endswith(".gz"):
             output_filename += ".gz"
         elif self.compression == "zstd" and not output_filename.endswith(".zstd"):
-            if output_filename.endswith(".parquet"):
-                output_filename = output_filename.replace(".parquet", ".zstd.parquet")
-                compression = None  # compression is already handled by ParquetWriter
-            else:
-                output_filename += ".zstd"
+            output_filename += ".zstd"
         self.max_file_size = max_file_size
         self.file_id_counter = Counter()
         if self.max_file_size > 0 and mode != "wb":
