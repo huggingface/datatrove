c416ca9d70aef62ae174a76b691fc5eac5dd68a3
justhungryman <justhungryman@gmail.com>
2024-06-23 00:18:13 +0900

    feat: Add zstd compression support for jsonl reader

--- tests/pipeline/test_jsonl_zstd_compression.py
+++ tests/pipeline/test_jsonl_zstd_compression.py
@@ -28,3 +28,19 @@ class TestZstdCompression(unittest.TestCase):
             assert read_doc == original
             c += 1
         assert c == len(data)
+
+    # def test_parquet_writer_reader(self):
+    #     data = [
+    #         Document(text=text, id=str(i), metadata={"somedata": 2 * i, "somefloat": i * 0.4, "somestring": "hello"})
+    #         for i, text in enumerate(["hello", "text2", "more text"])
+    #     ]
+    #     with ParquetWriter(output_folder=self.tmp_dir, compression="zstd") as w:
+    #         for doc in data:
+    #             w.write(doc)
+    #     reader = ParquetReader(self.tmp_dir)
+    #     c = 0
+    #     for read_doc, original in zip(reader(), data):
+    #         read_doc.metadata.pop("file_path", None)
+    #         assert read_doc == original
+    #         c += 1
+    #     assert c == len(data)
