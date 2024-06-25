08f07ed78a9bf0925e64abd7d6dd00bba09e90b8
Loubna Ben Allal <44069155+loubnabnl@users.noreply.github.com>
2024-06-25 17:10:19 +0200

    fix linting and add comment

--- examples/filter_hf_dataset.py
+++ examples/filter_hf_dataset.py
@@ -1,4 +1,12 @@
+"""
+This file contains code to:
+1 - Load a parquet-format Hugging Face dataset from the hub.
+2 - Filter the dataset (to include only entries that contain the word 'hugging' in the text column).
+3 - Push the filtered dataset back to the hub.
+"""
+
 import argparse
+
 
 parser = argparse.ArgumentParser("Filter an HF dataset and push the result to the hub")
 
@@ -14,8 +22,8 @@ LOCAL_LOGS_PATH = "my_local_logs_path"
 if __name__ == "__main__":
     args = parser.parse_args()
     from datatrove.executor import SlurmPipelineExecutor
-    from datatrove.pipeline.readers import ParquetReader
     from datatrove.pipeline.filters import LambdaFilter
+    from datatrove.pipeline.readers import ParquetReader
     from datatrove.pipeline.writers.huggingface import HuggingFaceDatasetWriter
 
     dist_executor = SlurmPipelineExecutor(
