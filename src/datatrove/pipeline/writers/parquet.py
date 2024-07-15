becf06251a9c2b022ad16b3bd4dac77e993125e1
sungjun lee <justHungryMan@gmail.com>
2024-07-16 01:05:23 +0900

    Update src/datatrove/pipeline/writers/parquet.py
    None to out of list
    
    Co-authored-by: Guilherme Penedo <nostrumg@gmail.com>

--- src/datatrove/pipeline/writers/parquet.py
+++ src/datatrove/pipeline/writers/parquet.py
@@ -14,7 +14,7 @@ class ParquetWriter(DiskWriter):
         self,
         output_folder: DataFolderLike,
         output_filename: str = None,
-        compression: Literal[None, "snappy", "gzip", "brotli", "lz4", "zstd"] | None = None,
+        compression: Literal["snappy", "gzip", "brotli", "lz4", "zstd"] | None = None,
         adapter: Callable = None,
         batch_size: int = 1000,
         expand_metadata: bool = False,
