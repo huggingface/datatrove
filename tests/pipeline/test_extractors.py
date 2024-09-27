c085736e8bae20b92f7475bd76292ac51f128b37
garrethlee <garreth.lee@huggingface.co>
2024-09-26 17:50:04 -0700

    feat: add tests

--- tests/pipeline/test_extractors.py
+++ tests/pipeline/test_extractors.py
@@ -1,8 +1,23 @@
 import unittest
 
-from datatrove.pipeline.extractors import Justext, ReadabilityInscriptis, Resiliparse, Trafilatura
+from datatrove.pipeline.extractors import (
+    Inscriptis,
+    Justext,
+    ReadabiliPy,
+    Readability,
+    ReadabilityInscriptis,
+    Resiliparse,
+    Trafilatura,
+)
 
-from ..utils import require_inscriptis, require_justext, require_readability, require_resiliparse, require_trafilatura
+from ..utils import (
+    require_inscriptis,
+    require_justext,
+    require_readabilipy,
+    require_readability,
+    require_resiliparse,
+    require_trafilatura,
+)
 
 
 ARTICLE_HTML = "<html><body><article><p>Hello World!</p></article></body></html>"
@@ -16,7 +31,7 @@ class TestExtractors(unittest.TestCase):
 
     @require_readability
     @require_inscriptis
-    def test_basic_article_readability(self):
+    def test_basic_article_readability_inscriptis(self):
         extractor = ReadabilityInscriptis(min_text_length=10, min_text_score=1)
         self.assertEqual(extractor.extract(ARTICLE_HTML), "Hello World!")
 
@@ -29,3 +44,19 @@ class TestExtractors(unittest.TestCase):
     def test_basic_article_resiliparse(self):
         extractor = Resiliparse()
         self.assertEqual(extractor.extract(ARTICLE_HTML), "Hello World!")
+
+    @require_readabilipy
+    def test_basic_article_readabilipy(self):
+        extractor = ReadabiliPy()
+        self.assertEqual(extractor.extract(ARTICLE_HTML), "Hello World!")
+
+    @require_inscriptis
+    def test_basic_article_inscriptis(self):
+        extractor = Inscriptis()
+        self.assertEqual(extractor.extract(ARTICLE_HTML), "Hello World!")
+
+    @require_readability
+    def test_basic_article_readability(self):
+        extractor = Readability(min_text_length=10, min_text_score=1)
+        postprocessor = Trafilatura()
+        self.assertEqual(extractor.extract(ARTICLE_HTML, postprocessor=postprocessor), "Hello World!")
