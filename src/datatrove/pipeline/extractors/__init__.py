0eb2f2a745c80ba07154dd87c77adb69e57bcf41
garrethlee <garreth.lee@huggingface.co>
2024-09-26 17:50:17 -0700

    feat: changed configs & pyproject

--- src/datatrove/pipeline/extractors/__init__.py
+++ src/datatrove/pipeline/extractors/__init__.py
@@ -1,4 +1,7 @@
+from .inscriptis import Inscriptis
 from .justext import Justext
 from .modular import ReadabilityInscriptis
+from .readabilipy import ReadabiliPy
+from .readability import Readability
 from .resiliparse import Resiliparse
 from .trafilatura import Trafilatura
