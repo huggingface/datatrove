f9035903c26021fdc8fd510ad0630d7a10d20f52
Vivien Cabannes <vivien.cabannes@gmail.com>
2025-04-29 19:50:28 +0800

    correct metadata parsing for finemath (#355)
    * correct metadata parsing for finemath
    
    * Update src/datatrove/pipeline/readers/base.py
    
    * Update src/datatrove/pipeline/readers/base.py
    
    * Update src/datatrove/pipeline/readers/base.py
    
    ---------
    
    Co-authored-by: Guilherme Penedo <nostrumg@gmail.com>

--- src/datatrove/pipeline/readers/base.py
+++ src/datatrove/pipeline/readers/base.py
@@ -58,11 +58,20 @@ class BaseReader(PipelineStep):
         Returns: a dictionary with text, id, media and metadata fields
 
         """
+        metadata = data.pop("metadata", {})
+        if isinstance(metadata, str):
+            import json
+            try:
+                metadata = json.loads(metadata)
+            except json.JSONDecodeError:
+                pass
+        if not isinstance(metadata, dict):
+            metadata = {"metadata": metadata}
         return {
             "text": data.pop(self.text_key, ""),
             "id": data.pop(self.id_key, f"{path}/{id_in_file}"),
             "media": data.pop("media", []),
-            "metadata": data.pop("metadata", {}) | data,  # remaining data goes into metadata
+            "metadata": metadata | data,  # remaining data goes into metadata
         }
 
     def get_document_from_dict(self, data: dict, source_file: str, id_in_file: int | str):
