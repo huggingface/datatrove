f845e658a0256d8862e61e4ce067fdb38a21b77e
Joel Niklaus <joel@hf.co>
2026-01-27 13:19:42 +0000

    ran formatter

--- tests/pipeline/test_hf_reader.py
+++ tests/pipeline/test_hf_reader.py
@@ -9,7 +9,9 @@ from ..utils import require_datasets
 class TestHuggingFaceReader(unittest.TestCase):
     def test_read_dataset(self):
         reader = HuggingFaceDatasetReader(
-            "truthfulqa/truthful_qa", dataset_options={"name": "generation", "split": "validation"}, text_key="question"
+            "truthfulqa/truthful_qa",
+            dataset_options={"name": "generation", "split": "validation"},
+            text_key="question",
         )
         data = list(reader())
         self.assertEqual(len(data), 817)
